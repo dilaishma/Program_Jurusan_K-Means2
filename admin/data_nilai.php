ac883099702f7058a113f0b2d32edafdb7a1f1a2
fnais <fadhilanaisyah@gmail.com>
2024-06-28 13:29:44 +0700

    update judul

--- admin/data_nilai.php
+++ admin/data_nilai.php
@@ -20,7 +20,7 @@ include 'src/header.php';
       <!-- Default box -->
       <div class="box">
         <div class="box-header with-border">
-          <h3 class="box-title">Data Nilai</h3>
+          <h3 class="box-title">Detail Data Penyakit</h3>
 
           <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"> <i class="fa fa-minus"></i></button>
